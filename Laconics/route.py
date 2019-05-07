03c4d44b40702676f5f725b0d0badcefc40a63c6
Ben Durmishllari <ben.durmishllari@gmail.com>
2019-05-08 00:24:06 +0100

    cleaning code on route.py. adding cooments

--- Laconics/route.py
+++ Laconics/route.py
@@ -105,7 +105,7 @@ def login():
             
             # this functionality it direct you always
             # on guideline page after of the log in.
-            # it an important step for the system to know
+            # it's an important step for the system to know
             # which is the direction after of the log in
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('guideline'))
@@ -161,6 +161,8 @@ def userprofile(id):
 @app.route('/userprofile/<int:id>/delete', methods=['POST'])
 def delete_user(id):
     
+    # try-except to dont allowed to delete a user 
+    # profile when its still exist expense for this author
     try:
 
         user = User.query.get_or_404(id)
@@ -179,20 +181,22 @@ def delete_user(id):
 
 
 @app.route('/register', methods=['GET', 'POST'])
+# @login_required
 def register():
     
-    # check the user if it's already log in to don't allow
-    # to to go on register page
-    """
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    """
-
+    
+    # saving on variable the register form
+    # you'll find this form on forms.py
     form = RegistrationForm()
     
     if form.validate_on_submit():
+
+        # hash password, functinality
+        # from the flask libraries you'll find it
+        # on __init__.py
         hased_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         
+        # creating the user object
         user = User(employee_number = form.employee_number.data, 
                     name = form.name.data,
                     surname = form.surname.data, 
@@ -200,11 +204,16 @@ def register():
                     password = hased_password, 
                     role = form.role.data)
         
+        
         db.session.add(user)
         db.session.commit()
         
         flash('Employee account has been created', 'success')
         return redirect(url_for('guideline'))
+    
+    # if statment to check the role of current user
+    # if the role is univen of admin
+    # it doesn't allowe you to access it.
     # if current_user.role != 'Admin':
     #     abort(403)
     return render_template('register.html', 
@@ -212,7 +221,8 @@ def register():
                             form=form)  
 
 
-
+# logout route, it's redirect you
+# on log in page
 @app.route('/logout')
 def logout():
     logout_user()
@@ -221,7 +231,7 @@ def logout():
 
 
 """
- This function it's save the pictures and it's giving secret characters to the images
+ This function it's save the pictures and it's rename and giving secret characters to the images
  that this characters are unique for each user. Also, it's resize the pictures
  that the user's upload on their profile in 256 pixel to save space and quality.
 """
@@ -239,15 +249,26 @@ def save_picture(form_picture):
     return picture_filename
 
 
+# route for the personal profile page 
+# on this page they have access only the current user
+# that its log in 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
     
+
+    # saving on variable the upldate profile form
+    # you'll find this form on forms.py
     form = UpdateProfileForm()
     
+
+
+    # on this functionality users have the access
+    # to update details of their info like name/email/password also, 
+    # to upload profile picture this method it's connect with the method 
+    # above I have more descriptions above the profile picture functionality above
     if form.validate_on_submit():
         
-
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
             current_user.profile_image = picture_file
@@ -271,7 +292,10 @@ def profile():
 
 
 
-
+# route for the expenses, this fuctionality it's show 
+# all the expenses that they own on current user
+# all the user can view their own expenses on the 
+# expenses option on the navigation bar
 @app.route('/expenses', methods=['GET', 'POST'])
 @login_required
 def expenses():
@@ -283,28 +307,52 @@ def expenses():
                             expenses=expenses)
 
 
-
+# route for the reports, this fuctionality it's show
+# all the expenses of the users only manager and admin accounts
+# have access for this. If the current user role is 'Manager' or 'Admin'
+# you can see the report option on the navigation bar otherwise this option is hiding.
 @app.route('/reports', methods=['GET', 'POST'])
 @login_required
 def reports():
+
     expenses = Expense.query.all()
+    
+    # if statment to be sure that if the 
+    # normal user try to access it the system will not
+    # allowed them
     if current_user.role == 'User':
         abort(403)
+    
     return render_template('reports.html', 
                             title='Reports', 
                             expenses=expenses)
     
-
+# route for the creation of the expenses
 @app.route('/create_expense/new', methods=['GET', 'POST'])
 @login_required
 def new_expense():
 
+    # saving on variable the upldate create expense form
+    # you'll find this form on forms.py
     form = CreateExpenseForm()
 
     if form.validate_on_submit():
 
+        # try for the creating of the expenses object
+        # I use this way because I set it to add on the database
+        # the receipt image as BLOB so the 'file' variable that you'll
+        # see after whatever it's reading it's send it on the database
+        # and it's creating the object including the receipt image.
+        # I use this way to manage this case of the 
+        # receipt image cause I wanted the full quality of the picture to be readable 
+        # and after when they delete the expense to remove it with the expense as part of it,
+        # to save space and to don't fill the db space with past files.
+
+        # except case is when the users create an expense without receipt image 
+        # to allow the system to create the object without this attribute.
         try:
-        
+            
+            
             file = request.files['picture_expense']      
             
             expense = Expense(client_name = form.client_name.data, 
@@ -318,7 +366,7 @@ def new_expense():
                               GBP = form.GBP.data, 
                               EUR = form.EUR.data, 
                               USD = form.USD.data, 
-                              receipt_image=file.read(),
+                              receipt_image = file.read(),
                               description = form.description.data,
                               author = current_user)
             expense.verify_or_decline = 'Pending'
@@ -350,6 +398,9 @@ def new_expense():
                             form=form)
 
 
+# route for the expenses profile
+# this route it's taking the expense id 
+# and it's display the expense with full details.
 @app.route('/expensesprofile/<int:expense_id>')
 def expensesprofile(expense_id):
     
@@ -357,12 +408,19 @@ def expensesprofile(expense_id):
 
         expense = Expense.query.get_or_404(expense_id)
         
+        # as I mention above the receipt images are saving 
+        # on the database as BLOB type, this variable it's decoding
+        # the images by using base64. I decode it and pass this variable into html with
+        # jinja to display the picture. I use this way to manage this case of the 
+        # receipt image cause I wanted the full quality of the picture to be readable 
+        # and after when they delete the expense to remove it with the expense as part of it
+        # to save space and to don't fill the db space with past files.
         image = base64.b64encode(expense.receipt_image)
 
         return render_template('expensesprofile.html', 
                                 expense=expense, 
                                 client_name=expense.client_name, 
-                                image=image.decode('utf-8'))
+                                image=image.decode('utf-8')) 
     
     except:
         
@@ -372,7 +430,10 @@ def expensesprofile(expense_id):
                                 expense=expense, 
                                 client_name=expense.client_name)
 
-
+# route for the expense status, this route
+# change the status of pending to verify also, it's accessable
+# only from the manager account as is the only role that 
+# the jinja on html display this buttons.
 @app.route('/expensesprofile/<int:expense_id>/verify', methods=['GET', 'POST'])
 def verify(expense_id):
     expense = Expense.query.get_or_404(expense_id)
@@ -387,7 +448,10 @@ def verify(expense_id):
                             expense=expense)
 
 
-
+# route for the expense status, this route
+# change the status of pending to decline also, it's accessable
+# only from the manager account as is the only role that 
+# the jinja on html display this buttons.
 @app.route('/expensesprofile/<int:expense_id>/decline', methods=['GET', 'POST'])
 def decline(expense_id):
     expense = Expense.query.get_or_404(expense_id)
@@ -401,14 +465,15 @@ def decline(expense_id):
                             title='Expenses', 
                             expense=expense)
 
-
+# route for the edit expense, this functionality is availabe
+# only for the admin account and when an expense is on pending status
 @app.route('/expensesprofile/<int:expense_id>/edit', methods=['GET', 'POST'])
 def edit_expense(expense_id):
     
     expense = Expense.query.get_or_404(expense_id)
     form = Edit_expenseForm()
 
-   
+    # adding the changes on the db
     if form.validate_on_submit():
         expense.receipt = form.receipt.data
         expense.expense_category = form.expense_category.data
@@ -419,22 +484,35 @@ def edit_expense(expense_id):
         db.session.commit()
         flash('Expense has been updated', 'success')
         return redirect(url_for('expensesprofile', expense_id=expense.expense_id))
+    
+    # keeping the details that are same
     elif request.method == 'GET':
+        form.receipt.data = expense.receipt
+        form.expense_category.data = expense.expense_category
         form.client_project.data = expense.client_project
         form.client_or_saggezza.data = expense.client_or_saggezza
+        form.billable_to.data = expense.billable_to
+        form.payment.data = expense.payment
+    
     if current_user.role != 'Admin':
         abort(403)
+    
     return render_template('edit_expense.html', 
                             title='Edit Expense', 
                             form=form, 
                             expense=expense)
 
-
+# route for the delete expense, this fuctionality is available
+# only for the admin account and it's deleting the whole expense
+# from the db by taking the expense id
 @app.route('/expensesprofile/<int:expense_id>/delete', methods=['POST'])
 def delete_expense(expense_id):
     
     expense = Expense.query.get_or_404(expense_id)
 
+    # again we check for security reason
+    # if the account role is Admin to give 
+    # access of this functionality
     if current_user.role != 'Admin':
         abort(403)
     db.session.delete(expense)
@@ -443,11 +521,24 @@ def delete_expense(expense_id):
     return redirect(url_for('reports'))
 
 
+# method for the email that the users receive
+# to reset their password via email, this method use 
+# the secret token character are always unique for any 
+# email that it will send it. Also, I set up the token to expire
+# after of 15 minutes for security reason you'll find those method
+# on the models.py into the user class etc. Finally on sender you need
+# to pass the email of main email that you'll set on mail(username) config on __init__.py
 def reset_email(user):
     token = user.get_reset_token()
     message = Message('Request to reset your password',
-                       sender = 'ben.durmishllari@gmail.com',
+                       sender = 'ben.durmishllari@gmail.com', 
                        recipients = [user.email])
+    
+    # into the mail body users will receive a link that it's start with
+    # the direction of 'reset_password.html' that it's the page with the form
+    # that it will allowed the user to set the new pass, after it's the token 
+    # characters as I mention above. All that are creating a link that it's
+    # give to user a safty way to change his password via email.
     message.body = f''' Please click on link bellow to reset your password:
 {url_for('change_password', token=token, _external=True)}
 
@@ -456,7 +547,10 @@ def reset_email(user):
 
     mail.send(message)
 
-
+# route for reset password via email that it's pass the user on a form
+# to input email. This functionality it's checking into database if this 
+# email exist on register user and if it's true it's sending the above mail 
+# to reset the passowrd.
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_password():
 
@@ -479,13 +573,20 @@ def reset_password():
                             title='Reset Password', 
                             form=form)
 
-
+# route for the change password, this route it's render
+# when the user click on the link that receive on the email
+# and it's giving the access to set the new password, this data
+# are replace into dabatase by giving the access to log in with
+# the new password.
 @app.route('/change_password/<token>', methods=['GET', 'POST'])
 def change_password(token):
     if current_user.is_authenticated:
         return redirect('guideline')
     user = User.verify_reset_token(token)
 
+    # if the link its expires the system
+    # it doesn't recognize the token so it's understand that this
+    # user might not exist or the mail is expired.
     if user is None:
         flash('This user might not exist or this email has been expired', 'danger')
         return redirect(url_for('reset_password'))
@@ -503,7 +604,16 @@ def change_password(token):
 
 
 
-
+# method of the expense that it's sending via email
+# when the expense it has verify status. This method is connected with
+# send_expense() by passing the expense id to know wich expense has to send.
+# This method have one requirement to work, admin needs to create a virtual
+# account and to set as email the email of the payment department of the company
+# and after to pass on the 'user' variable the possition that this account has on the
+# database. With this way we pass on the recepient the email of the company payment department,
+# users needs only to know the email of the payment department and to fill when it will render 
+# the form that it's asking for the email of the payment department. Finally on sender you need
+# to pass the email of main email that you'll set on mail(username) config on __init__.py
 def expensemail(expense_id):
     user = User.query.get(2)
     expense = Expense.query.get_or_404(expense_id)
@@ -536,7 +646,11 @@ def expensemail(expense_id):
     mail.send(message)
 
 
-
+# route for send expense via email, this method will direct users
+# on a form to fill the email of the company payment department. The only 
+# requirement for this fuctionality is that the users need to know the email of the
+# payment department because as I mention above the app will check into database if this
+# email exist.
 @app.route('/send_expense/<int:expense_id>/sendexpense', methods=['GET', 'POST'])
 @login_required
 def send_expense(expense_id):
